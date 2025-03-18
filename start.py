3671f28b51bf85be4d54c540e7b64926fce77c37
Mauricioosu <mauricioosuna3@gmail.com>
2025-03-17 20:56:52 -0400

    Adicionando Horario e limite de trasacoes

--- start.py
+++ start.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 print('''
       
     Seja bem vindo ao banco DIO
@@ -11,15 +13,15 @@ print('''
 
 saldo = 1000
 extrato = ('')
-quantidade_de_saques = 0
+quantidade_de_transacoes = 0
 
 while True:
     opcao = int(input('Digite a opção desejada:'))
 
     if opcao == 1:
 
-        if quantidade_de_saques >= 3:
-            print('Limite de saques diários atingido')
+        if quantidade_de_transacoes == 10:
+            print('Limite de trasacoes diários atingido')
 
         else:
             saque = int(input(' \n Digite o valor do saque: '))
@@ -32,19 +34,24 @@ while True:
             else:
                 saldo = saldo - saque
                 print(f' \n Saque realizado com sucesso, seu saldo é de R$: {saldo:.2f}')
-                extrato = extrato + f'Saque R$: -{saque:.2f}\n'
-                quantidade_de_saques = quantidade_de_saques + 1
-                print(f'Limite de saques diários restantes: {3 - quantidade_de_saques}')
+                extrato = extrato + f'Saque R$: -{saque:.2f}\n' + datetime.now().strftime('%d/%m/%Y %H:%M:%S') + '\n'
+                quantidade_de_transacoes += 1 
+                print(f'Limite de saques diários restantes: {10 - quantidade_de_transacoes}')
 
     elif opcao == 2:
-        print(' \n Deposito')
-        deposito = int(input('Digite o valor do deposito: '))
-        saldo = saldo + deposito
-        print(f'Deposito realizado com sucesso, seu saldo é de R$: {saldo:.2f}')
-        extrato = extrato + f'Deposito R$: +{deposito:.2f}\n'
+        if quantidade_de_transacoes == 10:
+            print('Limite de trasacoes diários atingido')
+        else:
+            print(' \n Deposito')
+            deposito = int(input('Digite o valor do deposito: '))
+            saldo = saldo + deposito
+            quantidade_de_transacoes += 1
+            print(f'Deposito realizado com sucesso, seu saldo é de R$: {saldo:.2f} . Voce ainda tem {10 - quantidade_de_transacoes} transações disponíveis')
+            extrato = extrato + f'Deposito R$: +{deposito:.2f}\n ' + datetime.now().strftime('%d/%m/%Y %H:%M:%S') + '\n'
+
 
     elif opcao == 3:
-        print(extrato + f' \n Seu Saldo é R$: {saldo:.2f}')
+        print(extrato + f' \n Seu Saldo é R$: {saldo:.2f} ')
 
     elif opcao == 4:
         break
